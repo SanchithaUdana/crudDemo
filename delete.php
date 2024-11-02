6279bcbe54e3770f2469cac6998cf3a6175819ae
Sanchitha Udana Wijesundara <109952575+SanchithaUdana@users.noreply.github.com>
2024-11-02 15:21:25 +0530

    con update

--- delete.php
+++ delete.php
@@ -5,7 +5,7 @@ $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
 
 if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
 
-$database = mysqli_select_db($connection, DB_DATABASE);
+$database = mysqli_select_db($conn, DB_DATABASE);
 
 $id = $_GET["id"];
 $sql = "DELETE FROM `crud` WHERE id = $id";
