f08c52f15b9e085ec3e3c772317dca1cd9cfbc2e
Sanchitha Udana Wijesundara <109952575+SanchithaUdana@users.noreply.github.com>
2024-11-02 19:31:01 +0530

    Update db.inc.php

--- db.inc.php
+++ db.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_SERVER', 'localhost');
+define('DB_SERVER', 'cruddemo.cr4qo8eowr5r.us-east-1.rds.amazonaws.com');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'Password2002');
 define('DB_DATABASE', 'crudDemo');
