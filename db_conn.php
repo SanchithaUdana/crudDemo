b69cf3efc52e28cd6d84267ca180d83460889c89
Sanchitha Udana Wijesundara <109952575+SanchithaUdana@users.noreply.github.com>
2024-11-02 14:18:46 +0530

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -1,11 +1,13 @@
 <?php
-$servername = "cruddemo.cr4qo8eowr5r.us-east-1.rds.amazonaws.com";
-$username = "root";
-$password = "Password2002";
-$dbname = "crudDemo";
+
+define('DB_SERVER', 'cruddemo.cr4qo8eowr5r.us-east-1.rds.amazonaws.com');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'Password2002');
+define('DB_DATABASE', 'crudDemo');
+
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
 
 // Check connection
 if (!$conn) {
